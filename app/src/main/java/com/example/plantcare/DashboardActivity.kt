4edea211f46e072226699e146f9da77c598ad345
shiva2552 <shiva.26622e@gmail.com>
2023-11-18 20:54:15 -0800

    adding the base for grid Adapter

--- app/src/main/java/com/example/plantcare/DashboardActivity.kt
+++ app/src/main/java/com/example/plantcare/DashboardActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.GridView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.plantcare.databinding.ActivityDashboardBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -15,6 +17,7 @@ class DashboardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDashboardBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var userEmail: String
+    private lateinit var gridView: GridView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
@@ -27,6 +30,15 @@ class DashboardActivity : AppCompatActivity() {
         userEmail = intent.getStringExtra(getString(R.string.user_email_intent_tag))!!
 
         binding.greetingTextView.text = "Hello, " + this.userEmail
+
+        gridView = findViewById(R.id.gridView)
+
+        var gridItemAdapter = GridItemAdapter(this)
+        gridView.adapter = gridItemAdapter
+        gridView.setOnItemClickListener { adapterView, parent, position, l ->
+            Toast.makeText(this, "Click on : $position", Toast.LENGTH_LONG).show()
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
