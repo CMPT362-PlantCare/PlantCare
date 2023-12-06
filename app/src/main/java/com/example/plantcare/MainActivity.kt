e0517c17f7d74896bc589b586dd724afe0a179e6
Divya Soneji <divya.soneji@hotmail.com>
2023-12-05 20:04:06 -0800

    Fix Splash Screen Bugs

--- app/src/main/java/com/example/plantcare/MainActivity.kt
+++ app/src/main/java/com/example/plantcare/MainActivity.kt
@@ -1,35 +1,24 @@
 package com.example.plantcare
 
-import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.LayoutInflater
 import android.view.View
-import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.plantcare.databinding.ActivitySignupBinding
 import com.google.android.material.button.MaterialButton
-import com.google.android.material.button.MaterialButtonToggleGroup
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-
+private const val ANIMATION_DELAY = 1000L
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivitySignupBinding
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var firebaseDatabase: FirebaseDatabase
     private lateinit var rellay1: RelativeLayout
     private lateinit var topAnim: Animation
     private lateinit var bottomAnim: Animation
@@ -38,66 +27,53 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnSignup: MaterialButton
     private lateinit var btnLogin: MaterialButton
     private var handler = Handler()
-    private var runnable = Runnable { rellay1!!.visibility = View.VISIBLE }
+    private var runnable = Runnable { rellay1.visibility = View.VISIBLE }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignupBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
         firebaseAuth = Firebase.auth
-        firebaseDatabase = Firebase.database
 
         handleUserAlreadyLoggedIn()
+        initializeViews()
+        setupAnimations()
+        setupButtonClickListeners()
+    }
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        ) //to hide status bar
-        setContentView(R.layout.activity_main)
+    private fun initializeViews() {
         rellay1 = findViewById(R.id.rellay1)
-
-        //Animations
-        topAnim = AnimationUtils.loadAnimation(
-            this,
-            R.anim.top_animation
-        ) //we are using top_animation in this context
-        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
-
-        //hooks
         logoImgview = findViewById(R.id.logoImgview)
         txtview = findViewById(R.id.txtview)
         btnSignup = findViewById(R.id.btnSignup)
         btnLogin = findViewById(R.id.btnLogin)
-        logoImgview.setAnimation(topAnim)
-        txtview.setAnimation(bottomAnim)
-        handler.postDelayed(runnable,1000)
-        val radioLsnr = View.OnClickListener { v ->
-            val slktdbtn = findViewById<View>(v.id) as MaterialButton
-            if (slktdbtn.text == getString(R.string.sign_up)) {
-                val intent = Intent(this, SignupActivity::class.java)
-                startActivity(intent)
-            }
-            if (slktdbtn.text == getString(R.string.login)) {
-                val intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
-            }
+    }
 
-            }
+    private fun setupAnimations() {
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        logoImgview.animation = topAnim
+        txtview.animation = bottomAnim
+        handler.postDelayed(runnable, ANIMATION_DELAY)
+    }
 
-        btnSignup.setOnClickListener(radioLsnr)
-        btnLogin.setOnClickListener(radioLsnr)
-
+    private fun setupButtonClickListeners() {
+        btnSignup.setOnClickListener{
+            startActivity(SignupActivity::class.java)
+        }
+        btnLogin.setOnClickListener{
+            startActivity(LoginActivity::class.java)
+        }
     }
 
     private fun handleUserAlreadyLoggedIn() {
         if (firebaseAuth.currentUser != null) {
-            firebaseAuth.currentUser.let { user ->
-                if (user != null) {
-                    val dashboardActivityIntent = Intent(this, DashboardActivity::class.java)
-                    startActivity(dashboardActivityIntent)
-                    finish()
-                }
-            }
+            startActivity(DashboardActivity::class.java)
+            finish()
         }
     }
 
+    private fun <T : Any> startActivity(activityClass: Class<T>) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
+    }
 }
